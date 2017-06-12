4e00f3d49896dba11f3a6826cde7d9d7f133954d
bethek <kreshnikiberdemaj@gmail.com>
2017-06-12 16:48:50 +0200

    Faire en sorte que l'environnement communique comme il se doit

--- Formulaire.php
+++ Formulaire.php
@@ -1,6 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
 <?php
 
- Formulaire
+$date = date("d-m-Y");
+	$heure = date("H:i");
+	Print("Nous sommes le $date et il est $heure");
 
 
-?>
+
+
+ ?>
+
+
+
+            <form action="Réponses.php" method="post" name="generateur_contenu">
+
+            <label for="form-name">To do tasks</label>
+            <input type="text" name="name" placeholder="Please enter your list" required="required" data-error="Task is required.">
+
+            <input type="submit" class="btn btn-success btn-send" value="Save Task">
+
+
+        </form>
+
+
+
+
+
+
+
+    </body>
+</html>
